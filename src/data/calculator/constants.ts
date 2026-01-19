532783f15415aac58b6219d1f6c9a442ede4b2bd
aaltatan <a.altatan@gmail.com>
2026-01-19 07:16:18 +0300

    new syrian pound

--- src/data/calculator/constants.ts
+++ src/data/calculator/constants.ts
@@ -1,15 +1,15 @@
 import type { Bracket } from "./models";
 
-export const DEFAULT_SALARY_VALUE: number = 1_000_000;
-export const MIN_SALARY_VALUE: number = 837_000;
+export const DEFAULT_SALARY_VALUE: number = 10_000;
+export const MIN_SALARY_VALUE: number = 8_370;
 export const BRACKETS: Bracket[] = [
-  { min: 0, max: 837_000, rate: 0 },
-  { min: 837_001, max: 850_000, rate: 0.11 },
-  { min: 850_001, max: 1_100_000, rate: 0.13 },
-  { min: 1_100_001, max: 50_000_000, rate: 0.15 },
+  { min: 0, max: 8_370, rate: 0 },
+  { min: 8_370, max: 8_500, rate: 0.11 },
+  { min: 8_500, max: 11_000, rate: 0.13 },
+  { min: 11_000, max: 500_000, rate: 0.15 },
 ];
 
-export const MIN_SS_SALARY_VALUE: number = 750_000;
+export const MIN_SS_SALARY_VALUE: number = 7_500;
 export const DEFAULT_SS_RATE: number = 0.07;
 
 export const ROUND_METHODS = {
@@ -22,7 +22,7 @@ export type RoundMethod = keyof typeof ROUND_METHODS;
 
 export const TAXES_ROUNDING_STATUS: boolean = true;
 export const TAXES_ROUNDING_METHOD: RoundMethod = "ceil";
-export const TAXES_ROUND_TO_NEAREST: number = 100;
+export const TAXES_ROUND_TO_NEAREST: number = 1;
 
 export const SS_ROUNDING_STATUS: boolean = true;
 export const SS_ROUNDING_METHOD: RoundMethod = "round";
